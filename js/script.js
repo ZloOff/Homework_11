f5f1639bd98cdb999bfe33cc6b2288130edb289f
Ruslan <mr.zlooff@gmail.com>
2022-08-27 08:03:13 +0300

    swiper

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      BulletClass: "Custom_bullet",
+      clickable:true,
+    },
+    
+  });
